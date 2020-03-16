141ba7c7f392f46970c8e2ac6c8bd173b6110df5
Jung Hong <junghong0512@gmail.com>
2020-03-16 15:55:55 +0900

    How to Use Styled Component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component, Fragment } from "react";
+// import "./App.css";
+import styled, { createGlobalStyle } from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+  }
+`;
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <GlobalStyle />
+        <Container>
+          <Button>Hello</Button>
+          <Button danger>World</Button>
+          <Anchor as="a" href="https://google.com">
+            Go to Google
+          </Anchor>
+        </Container>
+      </React.Fragment>
+    );
+  }
 }
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  background-color: #bdc3c7;
+`;
+
+const Button = styled.button`
+  border-radius: 50px;
+  padding: 5px;
+  min-width: 120px;
+  color: white;
+  font-weight: 600;
+  cursor: pointer;
+  &:active,
+  &:focus {
+    outline: none;
+  }
+  background-color: ${props => (props.danger ? "#e74c3c" : "#2ecc71")};
+`;
+
+const Anchor = styled(Button)`
+  text-decoration: none;
+`;
+
+// const Button = ({ danger }) => (
+//   <button
+//     className={danger ? "button button--success" : "button button--danger"}
+//   >
+//     Hello
+//   </button>
+// );
 
 export default App;
